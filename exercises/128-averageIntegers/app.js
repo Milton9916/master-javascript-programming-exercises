4ab55fc4a4a3937f89964e7c3e1d837b3d6a5ca7
Milton <miltonusechez@gmail.com>
2026-03-06 22:43:39 +0000

    función average usando sum

--- exercises/128-averageIntegers/app.js
+++ exercises/128-averageIntegers/app.js
@@ -1,11 +1,14 @@
-function average(arr) {
+function average(arr2) {
   // your code here
-  
+  let total = sum(arr2)
+  return total / arr2.length
 }
 
-function sum(arr) {
+function sum(arr1) {
   // your code here
-  
-}
+  let resultado = arr1.reduce((num1 , num2) => num1 + num2,0)
 
+  return resultado
+}
+ 
 console.log(average([1, 2])); // --> 1.5
