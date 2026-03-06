2fba406c808920699c7085f9572e35c772ee766c
Milton <miltonusechez@gmail.com>
2026-03-06 20:57:19 +0000

    Agrega función para obtener el string más corto

--- exercises/124-findShortestWordAmongMixedElements/app.js
+++ exercises/124-findShortestWordAmongMixedElements/app.js
@@ -1,6 +1,18 @@
 function findShortestWordAmongMixedElements(arr) {
     // your code here
-    
+    let Mcorto = null;
+    let str = arr.some(element => typeof element ===`string`)
+    if (!arr || !str ){
+        return ``;
+    }else{
+        for (let i of arr){
+            if (typeof i === `string` && (Mcorto === null || i.length < Mcorto.length)){
+                Mcorto = i; 
+            }
+            
+        } 
+    }
+    return Mcorto
 }
 
 let output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
