e07b0522c18e0bf76696c39bf71a89af5329bde1
Milton <miltonusechez@gmail.com>
2026-03-07 00:42:39 +0000

    ejercicio 136

--- exercises/135-computeFactorialOfN/app.js
+++ exercises/135-computeFactorialOfN/app.js
@@ -1,6 +1,11 @@
 function computeFactorialOfN(n) {
     // your code here
+    let factorial = 1;
+    for (let i = n; i>0; i--){
+        factorial = i * factorial
     
+    }
+    return factorial
 }
 
 let output = computeFactorialOfN(4);
--- exercises/136-sumDigits/app.js
+++ exercises/136-sumDigits/app.js
@@ -1,6 +1,20 @@
 function sumDigits(num) {
     // your code here
-    
+    let esNegativo = num < 0;
+    num = Math.abs(num);
+    let texto = String(num)
+    let suma = 0;
+
+    for (let i=0; i < texto.length; i++){
+        let digito = Number(texto[i]);
+        
+        if(esNegativo && i === 0){
+            suma = suma - digito
+        }else{
+            suma = suma + digito
+        }
+    }
+    return suma
 }
 
 let output = sumDigits(-316);
